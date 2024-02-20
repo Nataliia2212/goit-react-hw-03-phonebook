8206a3f4443e4cd02172792b5ab951fb2e2039c5
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-02-20 23:33:53 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,28 @@ import React, { Component } from 'react';
 import ContactsList from './ContactsList/ContactsList'
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
+import localStorage from 'helpers/localStorage';
 
+const CONTACTS = 'contacts';
 
 export default class App extends Component {
 	state = {
-		contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+		contacts: [],
     filter: '',
-	}
+  }
+  
+   componentDidMount () {
+    const contacts = localStorage.load(CONTACTS);
+    if(contacts) {
+      this.setState({contacts})
+    }
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.save(CONTACTS, this.state.contacts)
+    }
+  }
   
   addContact = (newContact) => {
     this.setState(prev => ({contacts: [...prev.contacts, newContact]}) )
